75e43d6dd9ab1d83a693e5806a33c90ef3012b1e
Stiofain <stiofainf@gmail.com>
2021-11-22 23:19:29 +0000

    Fix indexing, add timing

--- testing.py
+++ testing.py
@@ -1,17 +1,26 @@
 from time import time
 import numpy as np
 import pandas as pd
-from aggregations import roll_weighted_mean
+from aggregations import roll_weighted_mean, recursive_sma
 
 
-def cython_testing(arr: np.ndarray, w: int):
+def existing(arr: np.ndarray, w: int):
     weights = np.array([1] * w, dtype=np.float64)
     start = time()
     r = roll_weighted_mean(arr, weights, w)
     end = time()
+    print('ans:', np.array(r))
+    print(end - start)
+
+def recursive_version(arr: np.ndarray, w: int):
+    start = time()
+    r = recursive_sma(arr, w)
+    end = time()
+    print('ans:', np.array(r))
     print(end - start)
 
 if __name__ == "__main__":
-    arr = np.array(range(1000000), dtype=np.float64)
+    arr = np.array(range(10000000), dtype=np.float64)
     w = 4
-    cython_testing(arr, w)
+    existing(arr, w)
+    recursive_version(arr, w)
